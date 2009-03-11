59cb56d3a4152244d96aebbf45fe72c0b4d01b4c
Vlad Seryakov <vseryakov@gmail.com>
2009-03-10 22:31:05 -0400

    initial import

---
+++ Makefile
@@ -0,0 +1,32 @@
+ifndef NAVISERVER
+    NAVISERVER  = /usr/local/ns
+endif
+
+ifndef EXPAT
+    EXPAT = /usr
+endif
+
+MOD      =  nsexpat.so
+OBJS     =  nsexpat.o tclexpat.o
+HDRS     =
+MODLIBS  =  -L$(EXPAT)/lib -lexpat
+
+CFLAGS   =  -I$(EXPAT)/include
+
+include  $(NAVISERVER)/include/Makefile.module
+
+nsexpat.c: check-env
+
+.PHONY: check-env
+
+check-env:
+	@if [ "$(EXPAT)" = "" ]; then \
+	    echo "** "; \
+	    echo "** EXPAT variable not set."; \
+	    echo "** expat.so will not be built."; \
+	    echo "** Usage: make EXPAT=/path/to/expat"; \
+	    echo "** Usage: make install EXPAT=/path/to/expat INST=/path/to/aolserver"; \
+	    echo "** "; \
+	    exit 1; \
+	fi
+
---
+++ nsexpat.c
@@ -0,0 +1,73 @@
+/*
+ * tclexpat.c --
+ *
+ *	A Tcl interface to James Clark's expat XML parser
+ *
+ * Copyright (c) 1998 Steve Ball, Zveno Pty Ltd
+ *
+ * Zveno Pty Ltd makes this software and associated documentation
+ * available free of charge for any purpose.  You may make copies
+ * of the software but you must include all of this notice on any copy.
+ *
+ * Zveno Pty Ltd does not warrant that this software is error free
+ * or fit for any purpose.  Zveno Pty Ltd disclaims any liability for
+ * all claims, expenses, losses, damages and costs any user may incur
+ * as a result of using, copying or modifying the software.
+ *
+ * $Id: nsexpat.c,v 1.3 2007/04/25 21:28:41 seryakov Exp $
+ *
+ */
+
+#define USE_TCL8X
+
+#include <ns.h>
+#include <tcl.h>
+#include "tclexpat.h"
+
+int Ns_ModuleVersion = 1;
+
+/*
+ *----------------------------------------------------------------------------
+ *
+ * TclExpat_Init --
+ *
+ *	Initialisation routine for loadable module
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	Defines "expat" command in the interpreter.
+ *
+ *----------------------------------------------------------------------------
+ */
+
+int Tclexpat_Init(Tcl_Interp * interp, void *context)
+{
+    Tcl_CreateObjCommand(interp, "expat", TclExpatObjCmd, NULL, NULL);
+    return TCL_OK;
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Ns_ModuleInit --
+ *
+ *	Load the config parameters, setup the structures, and
+ *	listen on the trap port.
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	None
+ *
+ *----------------------------------------------------------------------
+ */
+
+int Ns_ModuleInit(char *hServer, char *hModule)
+{
+    Ns_TclRegisterTrace(hServer, Tclexpat_Init, 0, NS_TCL_TRACE_CREATE);
+    return NS_OK;
+}
+
